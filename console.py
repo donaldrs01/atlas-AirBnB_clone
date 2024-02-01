d549b58f36c8a843d67921c7884b42ee72b2b14d
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-02-01 19:43:00 +0000

    basic cmd interpreter

---
+++ console.py
@@ -0,0 +1,25 @@
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand - basic command-line interpreter
+    """
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """
+        Quit the HBNBCommand interpreter
+
+        Args:
+            -"Quit" or "EQF" - to exit interpreter
+
+        Returns:
+            - True to exit
+        """
+        return True
+
+    do_EQF = do_quit  # alias for 'quit' command
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
