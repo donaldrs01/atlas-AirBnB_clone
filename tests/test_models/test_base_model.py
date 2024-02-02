917a7c30b5dce0c0a0d9dc0d27f68115896c4e52
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-02-02 21:06:43 +0000

    base test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,15 +1,34 @@
-#!/usr/bin/python3
+import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
 
-    def instance_creation_blank(self):
+    def test_instance_construct_blank(self):
+        """
+        Tests instance construction with no args
+        """
         b = BaseModel()
         self.assertIsInstance(b, BaseModel)
         self.assertIsNotNone(b.id)
         self.assertIsNotNone(b.created_at)
         self.assertIsNotNone(b.updated_at)
 
-    def instance_creation_with_args(self):
+    def test_instance_creation_with_args(self):
+        """
+        Tests instance construction with provided args
+        """
+        timestamp = datetime.today()
+        custom_id = 75
+        
+        b = BaseModel(
+            id = custom_id,
+            created_at = timestamp,
+            updated_at = timestamp
+        )
+
+        self.assertEqual(b.id, 75)
+        self.assertEqual(b.created_at, timestamp)
+        self.assertEqual(b.updated_at, timestamp)
         
