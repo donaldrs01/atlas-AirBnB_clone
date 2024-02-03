62e7a1d75eb8603e5af360926822599eeba788b7
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-02-03 20:29:24 +0000

    file_stoarge documentation

---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import json
+
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes JSON file to
+    instances
+
+    Private class attributes:
+        __file_path (str) : path to the JSON file (ex: file.json)
+        __objects (dict) : empty but will store all objects by
+        <class name>.id
+            Ex: BaseModel obj with id=12121212 -> BaseModel.12121212
+    
+    Public instance methods:
+        all(self): returns dictionary containing all __objects
+        new(self, obj): adds new object to __objects dictionary
+            Uses the class name and object's 'id' as the key
+        save(self): serializes the __objects dictionary into JSON file
+        reload(self): deserializes (reloads) JSON file back into 
+        __objects dict
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Method that returns a dictionary containing all objects stored
+        in the __objects attribute
+
+        Returns:
+            dict: keys are in format <class name>.<id> and values are 
+            corresponding objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Adds new object to __objects dictionary
+        Uses the class name and object's ID as the key
