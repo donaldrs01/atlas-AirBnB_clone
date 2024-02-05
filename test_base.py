8ae38be5cfb5f4e66bba27140e40da6b3e8e6d92
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-02-05 20:47:30 +0000

    test programs

---
+++ test_base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+test_model = BaseModel()
+test_model.name = "Test model"
+test_model.number = 40
+print(test_model)
+test_model_json = test_model.to_dict()
+print(test_model_json)
+print("JSON of test:")
+for key in test_model_json.keys():
+    print(f"{key}: ({type(test_model_json[key])}) - {test_model_json[key]}")
