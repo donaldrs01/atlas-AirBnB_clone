24e532f526cc69915f654b32d6bfbbe311c2bff6
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-07 14:06:04 -0600

    pep8 test check

--- console.py
+++ console.py
@@ -179,5 +179,6 @@ class HBNBCommand(cmd.Cmd):
             setattr(instance, attribute_name, attribute_value)
             storage.save()  # saves updated instance into JSON file
 
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
