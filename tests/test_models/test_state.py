33e2fa5f3088bb6613af69d97205c4051d95bbfa
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-12 03:37:21 -0600

    testing

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
 """Unit test module for State class"""
 import unittest
-import models
-from models.engine.file_storage import FileStorage
 from models.state import State
 
 
 class TestState(unittest.TestCase):
     """Tests correct construction of State instances"""
-    def test_is_instance(self):
-        test = State()
-        self.assertIsInstance(test, State)
+    def test_state_declare(self):
+        self.a_state = State
 
-    def test_store(self):
-        test = State()
-        self.assertIn(test, models.storage.all().values())
-
-    def test_unique_IDs(self):
-        test1 = State()
-        test2 = State()
-        self.assertNotEqual(test1.id, test2.id)
+    def test_state_input(self):
+        self.assertEqual(self.a_state.name, "")
 
 if __name__ == '__main__':
     unittest.main()
