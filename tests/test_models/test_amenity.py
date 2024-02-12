9c5fdc44c234a8e19b8c09f1a35416be0153174a
NolbsAtHolbs <hikikomode@gmail.com>
2024-02-12 08:45:43 -0600

    pep

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -7,9 +7,15 @@ from models.amenity import Amenity
 class TestAmenity(unittest.TestCase):
     """Tests correct construction of Amenity instances"""
     def test_amenity_declare(self):
-        """unittest test test class"""
         an_amenity = Amenity()
-        self.assertTrue(hasattr(an_amenity, 'name'))
+        self.assertTrue(hasattr(an_amenity, "name"))
+
+    def test_amenity_attributes(self):
+        an_amenity = Amenity()
+        self.assertTrue(hasattr(an_amenity, "name"))
+        self.assertEqual(an_amenity.name, "")
+        an_amenity.name = "Skylight"
+        self.assertEqual(an_amenity.name, "Skylight")
 
 if __name__ == '__main__':
     unittest.main()
